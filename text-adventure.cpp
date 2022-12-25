88774ee130f872c9165039889175e20bd17a2f88
Isaiah Earley <bbsian.135@gmail.com>
2022-12-25 15:05:39 -0800

    Fixed bugs that caused issues with character select. Added additional story plot.

--- text-adventure.cpp
+++ text-adventure.cpp
@@ -1,36 +1,63 @@
 #include <iostream>
+#include <stdlib.h>
 
 int main(){
 
 //variables
     //character select
     // florence
-int main_char = 1; 
-int main_char2 = 2;
+int main_char;
+int Florence;
+int Isabella;
 
 //instructions
-std::cout << "Welcome to my story selector. This story is able to be played by selecting number keys 1 through 4 
-with a variety of endings.";
+std::cout << "\n\nWelcome to my text-adventure.\n" 
+<< "--------------------------------\n"
+<< "This game is played best using a keyboard with functional number keys.\n" 
+<< "You select numbers progressively which will ultimately effect\n"
+<< "your outcome and describe what type of person you are.\n";
 
 
 
 //select character 
 std::cout << "\nSelect a character: \n";
-std::cout << "1) Florence   " << "2) Isabella\n";
+std::cout << "1) Florence       " << "2) Isabella\n";
+std::cout << "--------------------------------\n";
 std::cin >> main_char;
 
-if (1 == main_char){
-    std::cout << "You've selected Florence! \n";
-} else if (2 == main_char2){
-    std::cout << "You've selected Isabella! \n";
+if (main_char == 1){
+    std::cout << "\nYou've selected Florence! \n";
+Florence = true;
+Isabella = false;
+} else if (main_char == 2){
+    std::cout << "\nYou've selected Isabella! \n";
+Isabella = true; 
+Florence = false;
 } else {
     std::cout << "Invalid Response.";
-    return 0;
 }
 
-//story line
-std::cout << "Once upon a time a... ";
 
+
+
+//story line
+
+std::cout << "\n--------------------------------\n";
+std::cout << "Once upon a time ";
+
+
+
+// florence
+if(Florence == true){
+    std::cout << "Florence";
+} 
+
+
+
+// isabella
+else if(Isabella == true){
+    std::cout << "Isabella";
+}
 
 
 
