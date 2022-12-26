fa45c11a7d17dd94dce5fb8cd0906201f2e8fd52
Isaiah Earley <bbsian.135@gmail.com>
2022-12-25 17:15:06 -0800

    Fixed bug after 2 hours where randomizer was only working once but not didn't randomize when loop was repeating - removed additional if statement which caused conflicts
    Added additional options for players to select when fighting bear enemy.

--- text-adventure.cpp
+++ text-adventure.cpp
@@ -76,28 +76,24 @@ std::cout << "----------------------------------------------------------------\n
 std::cout << "You have 5 options you are able to use to fend off this beast...\n";
 std::cout << "----------------------------------------------------------------\n";
 
-srand (time(NULL));
+
 int choice; 
-int bearHealth = 30;
-int bearAttack = rand() % 8 + 1; 
-
+int bearHealth = 50;
 int isabellaHealth = 23;
-int isabellaAttack = rand() % 3 + 2;
-
 int florenceHealth = 17;
-int florenceAttack = rand() % 7;
+
 
 // for loop until bear or player health drops to 0 or less.
-while (bearHealth >= 0){
-
-
-    srand (time(NULL));
-
-
+while (bearHealth >= 0){ 
     if (Florence == true){
-        while (florenceHealth != 0){
-            if(Florence == true){
-                    std::cout << "Health: " << florenceHealth << "    DMG: 1-7\n\n";
+        while (florenceHealth >= 0){
+
+            //character vars
+                srand (time(NULL));
+                int florenceAttack = rand() % 7;
+
+            //user select    
+                    std::cout << "Health: " << florenceHealth << "    DMG: 0-7\n\n";
                     std::cout << " 1) Attack \n";
                     std::cout << " 2) Defend \n";
                     std::cout << " 3) Adrenaline \n";
@@ -105,55 +101,69 @@ while (bearHealth >= 0){
                     std::cout << " 5) Flee \n";
                     std::cout << "Select choice here: ";
                     std::cin >> choice;
-
-                if(choice == 1){
-                    std::cout << "\nYou chose Attack!\n";
-                    bearHealth = bearHealth - florenceAttack;
-                    std::cout << "You did " << florenceAttack << " damage to the enemy!\n\n";
-                    std::cout << bearHealth << "\n";
-                } 
-            } 
-        }
-    } else if (Isabella == true){
-    while (isabellaHealth != 0){
-        if (Isabella == true){
-                std::cout << "Health: " << isabellaHealth << "     DMG : 2-3\n\n";
-                std::cout << " 1) Attack \n";
-                std::cout << " 2) Defend \n";
-                std::cout << " 3) Infatuate \n";
-                std::cout << " 4) Items \n";
-                std::cout << " 5) Flee \n";
-                std::cout << "Select choice here: ";
-                std::cin >> choice;
+        //attack
+            if(choice == 1){
+                std::cout << "\nYou chose Attack!\n";
+                bearHealth = bearHealth - florenceAttack;
+                std::cout << "You did " << florenceAttack << " damage to the enemy!\n\n";
+                std::cout << bearHealth << "\n";
+            } else if (choice == 2){
+                std::cout << "\nYou chose defend!\n";   
+            } else if (choice == 3){
+                std::cout << "\nYou chose Adreneline!\n";
+            } else if (choice == 4){
+                std::cout << "\nItem Select\n";
+            } else if (choice == 5){
+                std::cout << "\nYou chose Flee!\n";
+            } else {
+                std::cout << "\nInvalid Choice! Defending.\n\n";
             }
         }
+    } else if (Isabella == true){
+        while (isabellaHealth >= 0){
+
+
+
+/* Infatuation gives better flee chance, 
+and lowers enemies damage (rng rate if 
+successful 1/3 chance.)*/
+
+            //character vars
+                srand (time(NULL));
+                int isabellaAttack = rand() % 4 + 2;
+
+            //user select    
+                    std::cout << "Health: " << isabellaHealth << "     DMG : 0-5\n\n";
+                    std::cout << " 1) Attack \n";
+                    std::cout << " 2) Defend \n";
+                    std::cout << " 3) Infatuate \n";
+                    std::cout << " 4) Items \n";
+                    std::cout << " 5) Flee \n";
+                    std::cout << "Select choice here: ";
+                    std::cin >> choice;
+
+        //attack
+            if (choice == 1){
+                bearHealth = bearHealth - isabellaAttack;
+                std::cout << "\nYou did " << isabellaAttack << " damage to the enemy!\n";
+                std::cout << bearHealth << "\n";
+            } else if (choice == 2){
+                std::cout << "\nYou chose defend!\n";   
+            } else if (choice == 3){
+                std::cout << "\nYou chose Infatuate!\n";
+            } else if (choice == 4){
+                std::cout << "\nItem Select\n";
+            } else if (choice == 5){
+                std::cout << "\nYou chose Flee!\n";
+            } else {
+                std::cout << "\nInvalid Choice! Defending.\n\n";
+            }
+
+
+        }
     }
-//attack 
-    if (choice == 1 && Isabella == true) {
-        std::cout << "\nYou did " << isabellaAttack << "to the enemy!\n";
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
-
 
 system("pause");
 return 0;
 }
-
-
